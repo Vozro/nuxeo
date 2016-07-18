d6614a5fcac2996356fc93c4e1fdaf142a74212b
Ricardo Dias <rdias@nuxeo.com>
2016-07-18 17:02:52 +0100

    NXP-16889: Add new commandline LibreOffice converter.
    Deprecate the use of JOD based converter in favor of the new converter.
    
    OOoManager is now disabled by default and requires a config contribution to be enabled.

---
+++ nuxeo-services/nuxeo-platform-convert/src/main/java/org/nuxeo/ecm/platform/convert/tester/DefaultCommandTester.java
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Ricardo Dias
+ */
+package org.nuxeo.ecm.platform.convert.tester;
+
+import org.apache.commons.lang.StringUtils;
+import org.nuxeo.ecm.core.commandline.executor.service.CommandLineDescriptor;
+import org.nuxeo.ecm.core.commandline.executor.service.cmdtesters.CommandTestResult;
+import org.nuxeo.ecm.core.commandline.executor.service.cmdtesters.CommandTester;
+
+import java.io.IOException;
+
+/**
+ * Simple {@link CommandTester} that accepts a command to test the existence of the command line.
+ * 
+ * @since 8.4
+ */
+public class DefaultCommandTester implements CommandTester {
+
+    @Override
+    public CommandTestResult test(CommandLineDescriptor cmdDescriptor) {
+        String cmd = cmdDescriptor.getCommand();
+        String arguments = cmdDescriptor.getTesterParameters().get("arguments");
+        try {
+            Runtime.getRuntime().exec(StringUtils.join(new String[] { cmd, arguments }, " "));
+        } catch (IOException e) {
+            return new CommandTestResult("command " + cmdDescriptor.getCommand()
+                    + " not found in system path (descriptor " + cmdDescriptor + ")");
+        }
+        return new CommandTestResult();
+    }
+}
